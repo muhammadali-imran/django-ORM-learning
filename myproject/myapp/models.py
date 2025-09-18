9383314d49f3e030918e2ebbb1d46e88eb72a945
muhammadali-imran <imranmuhammadali633@gmail.com>
2025-09-18 23:00:35 +0500

    Created some new tables and relations between them

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=100)   # String field
-    age = models.IntegerField()               # Integer field
-    email = models.EmailField(unique=True)    # Unique email field
-    enrolled_date = models.DateField(auto_now_add=True)  # Automatically set when created
+    name = models.CharField(max_length=100)   
+    age = models.IntegerField()               
+    email = models.EmailField(unique=True)   
 
     def __str__(self):
-        return f"{self.name} ({self.age})"
+        return "{} ({})".format(self.name,self.email)
+    
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.Textfield(max_length=350)
+
+    def __str__(self):
+        return "{} - {}".format(self.code, self.title)
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment_date = models.DateField(auto_now_add =True)
+
+    def __str__(self):
+        return "{} enrolled in {}".format(self.student.name, self.course.code)
