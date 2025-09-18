6c9dea687e5f55908b2b0ca82477d7ca4c5d9f16
muhammadali-imran <imranmuhammadali633@gmail.com>
2025-09-18 20:12:48 +0500

    Created student model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)   # String field
+    age = models.IntegerField()               # Integer field
+    email = models.EmailField(unique=True)    # Unique email field
+    enrolled_date = models.DateField(auto_now_add=True)  # Automatically set when created
+
+    def __str__(self):
+        return f"{self.name} ({self.age})"
