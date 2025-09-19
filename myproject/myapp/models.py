bbc905a5769ccf412d167cccde19ff03fa4ad0f9
muhammadali-imran <imranmuhammadali633@gmail.com>
2025-09-19 12:44:48 +0500

    Commiting the entire sehema of DB

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,18 +1,35 @@
 from django.db import models
 
-# Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length=100)   
-    age = models.IntegerField()               
-    email = models.EmailField(unique=True)   
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
-        return "{} ({})".format(self.name,self.email)
-    
+        return "{} ({})".format(self.name, self.code)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.department.name if self.department else "No Dept")
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.email)
+
 class Course(models.Model):
     title = models.CharField(max_length=100)
     code = models.CharField(max_length=20, unique=True)
-    description = models.Textfield(max_length=350)
+    description = models.CharField(max_length=350)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return "{} - {}".format(self.code, self.title)
@@ -20,7 +37,7 @@ class Course(models.Model):
 class Enrollment(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    enrollment_date = models.DateField(auto_now_add =True)
+    enrollment_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return "{} enrolled in {}".format(self.student.name, self.course.code)
